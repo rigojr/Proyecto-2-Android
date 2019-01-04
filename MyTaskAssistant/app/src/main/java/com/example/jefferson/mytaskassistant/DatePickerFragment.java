78651557727a2a4008c3ded4522d47d3f769f430
jjsislema <jeffersonsislema@gmail.com>
2019-01-04 06:09:09 -0400

    picker de la fecha listo falta el de la hora y dividir el atributo fecha de la clase task en fecha y hora

--- MyTaskAssistant/app/src/main/java/com/example/jefferson/mytaskassistant/DatePickerFragment.java
+++ MyTaskAssistant/app/src/main/java/com/example/jefferson/mytaskassistant/DatePickerFragment.java
@@ -2,14 +2,17 @@ package com.example.jefferson.mytaskassistant;
 
 
 import android.app.DatePickerDialog;
-import android.app.DialogFragment;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.DatePicker;
-import android.widget.TextView;
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
+
+
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,22 +20,23 @@ import android.widget.TextView;
 public class DatePickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener{
 
-
-    public DatePickerFragment() {
-        // Required empty public constructor
-    }
-
-
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        // Create a new instance of DatePickerDialog and return it.
+        return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Set the activity to the Main Activity.
+        DetailActivity activity = (DetailActivity) getActivity();
+        // Invoke Main Activity's processDatePickerResult() method.
+        activity.processDatePickerResult(year, month, day);
     }
 }
--- MyTaskAssistant/app/src/main/java/com/example/jefferson/mytaskassistant/DetailActivity.java
+++ MyTaskAssistant/app/src/main/java/com/example/jefferson/mytaskassistant/DetailActivity.java
@@ -1,25 +1,32 @@
 package com.example.jefferson.mytaskassistant;
 
 import android.app.DatePickerDialog;
-import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private EditText taskTitulo ;
+    private EditText taskFecha ;
+    private EditText taskDetalle ;
+    private CheckBox taskCompletado ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        EditText taskTitulo = findViewById(R.id.inputTitulo);
-        EditText taskfecha = findViewById(R.id.inputFecha);
-        EditText taskDetalle = findViewById(R.id.inputDetail);
-        CheckBox taskCompletado = findViewById(R.id.completadoDetail);
+        taskTitulo = findViewById(R.id.inputTitulo);
+        taskFecha = findViewById(R.id.inputFecha);
+        taskDetalle = findViewById(R.id.inputDetail);
+        taskCompletado = findViewById(R.id.completadoDetail);
 
 
         //obttener datos del intent
@@ -29,6 +36,16 @@ public class DetailActivity extends AppCompatActivity {
 
 
     public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.date_picker));
+    }
 
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (day_string + "/" + month_string + "/" + year_string);
+
+        taskFecha.setText(dateMessage);
     }
 }

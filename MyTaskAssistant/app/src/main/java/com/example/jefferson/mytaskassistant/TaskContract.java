97ba57be5fd65d80ead562e596f75ac2c7fd409e
fabian graterol <fabiancgraterol@gmail.com>
2019-01-04 23:46:53 -0400

    creando TaskContract

---
+++ MyTaskAssistant/app/src/main/java/com/example/jefferson/mytaskassistant/TaskContract.java
@@ -0,0 +1,15 @@
+package com.example.jefferson.mytaskassistant;
+
+import android.provider.BaseColumns;
+
+public class TaskContract{
+
+    public static abstract class TaskEntry implements BaseColumns {
+        public static final String TABLE_NAME = "task";
+
+        public static final String TITULO = "titulo";
+        public static final String DETALLE = "detalle";
+        public static final String FECHA = "fecha";
+        public static final String COMPLETADO = "completado";
+    }
+}
